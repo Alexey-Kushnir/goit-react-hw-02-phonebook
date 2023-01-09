40bedce8c3f75dc32ea9ba7d16e455340fd1bb96
Alexey-Kushnir <al7787@gmail.com>
2023-01-09 13:59:18 +0200

    new components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Section } from './Section/Section';
+
+// model.id = nanoid();
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          // height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Phonebook">
+          <ContactsInput />
+        </Section>
+        <Section title="Contacts">
+          <ContactsStorage />
+        </Section>
+      </div>
+    );
+  }
+}
